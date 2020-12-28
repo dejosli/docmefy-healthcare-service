7f77ebe3e3fa91f609dce8f8110d5acac7b6f9b8
Josli De Silva <30972853+dejosli@users.noreply.github.com>
2020-12-28 12:55:33 +0600

    updating homepage view

--- docmefy_healthcare/docmefy/views.py
+++ docmefy_healthcare/docmefy/views.py
@@ -3,6 +3,6 @@ from django.shortcuts import render
 # Create your views here.
 
 def home(request):
-    template_name = 'base.html'
+    template_name = 'home.html'
     context = {}
     return render(request, template_name, context)
